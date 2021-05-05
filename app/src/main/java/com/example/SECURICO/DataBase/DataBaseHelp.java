10e250a747f0066c232f1e8c6a870664396979fd
azzadpandit <azzdpandit0@gmail.com>
2021-05-05 15:11:55 +0530

    SECURICO app code

---
+++ app/src/main/java/com/example/SECURICO/DataBase/DataBaseHelp.java
@@ -0,0 +1,67 @@
+package com.example.SECURICO.DataBase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseHelp extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION= 1;
+    public static final String DATABASE_NAME = "run.db";
+    public static final String TABLE_NAME = "Student_table";
+    public static final String COL_1="ID";
+    public static final String COL_2="CUSTOMER_NAME";
+    public static final String COL_3="PHONE";
+    public static final String COL_4="EMAIL";
+    public static final String COL_5="ADDRESS";
+    public static final String COL_6="USERNAME";
+    public static final String COL_7="PASSWORD";
+
+    public DataBaseHelp(Context context)
+    {
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+ TABLE_NAME+"( ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "CUSTOMER_NAME TEXT," +
+                "PHONE TEXT," +
+                "EMAIL TEXT," +
+                "ADDRESS TEXT ," +
+                "USERNAME TEXT," +
+                "PASSWORD TEXT" +
+                ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists "+TABLE_NAME);
+        db.close();
+    }
+    public Boolean addUser(String customername,String phone,String email,String address,String username,String password)
+    {
+
+        SQLiteDatabase db= this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(COL_2,customername);
+        contentValues.put(COL_3,phone);
+        contentValues.put(COL_4,email);
+        contentValues.put(COL_5,address);
+        contentValues.put(COL_6,username);
+        contentValues.put(COL_7,password);
+        Long success= db.insert(TABLE_NAME,null,contentValues);
+        if (success==-1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+}
+
+
+
--- app/src/main/java/com/example/SECURICO/Fregment/Fragment_Sign_up.java
+++ app/src/main/java/com/example/SECURICO/Fregment/Fragment_Sign_up.java
@@ -10,13 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.SECURICO.DataBase.DataBaseHelp;
+import com.example.SECURICO.DataBase.userdetails;
 import com.example.SECURICO.R;
 
 public class Fragment_Sign_up extends Fragment {
-
     Button Signin;
     EditText cus_customername,cus_phone,cus_email,cus_address,cus_username,cus_password;
+    DataBaseHelp databasehelp;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,10 +31,7 @@ public class Fragment_Sign_up extends Fragment {
             @Override
             public void onClick(View v) {
                 regitionmethod();
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.mainlayout, new Fragment_Sign_in());
-                ft.addToBackStack(null);
-                ft.commit();
+
             }
         });
         cus_customername =view.findViewById(R.id.editTextTextPersonName);
@@ -39,7 +40,10 @@ public class Fragment_Sign_up extends Fragment {
         cus_address=view.findViewById(R.id.editTextAddress);
         cus_username=view.findViewById(R.id.editTextUserName);
         cus_password=view.findViewById(R.id.editTextPassword);
-    return view;
+
+        databasehelp=new DataBaseHelp(getContext());
+
+        return view;
     }
 
     private void regitionmethod() {
@@ -49,10 +53,49 @@ public class Fragment_Sign_up extends Fragment {
         String str_address=cus_address.getText().toString();
         String str_username=cus_username.getText().toString();
         String str_password=cus_password.getText().toString();
+
         if (str_customername.isEmpty())
         {
             cus_customername.setError("customer name");
             cus_customername.requestFocus();
         }
+        if (str_phone.isEmpty())
+        {
+            cus_phone.setError("Phone number");
+            cus_phone.requestFocus();
+        }
+        if (str_email.isEmpty()) {
+            cus_email.setError("customer name");
+            cus_email.requestFocus();
+        }
+        if (str_address.isEmpty()) {
+            cus_address.setError("customer name");
+            cus_address.requestFocus();
+        }
+        if (str_username.isEmpty()) {
+            cus_username.setError("customer name");
+            cus_username.requestFocus();
+        }
+        if (str_password.isEmpty()) {
+            cus_password.setError("customer name");
+            cus_password.requestFocus();
+        }
+        else
+        {
+            Boolean test = databasehelp.addUser(str_customername,str_phone,str_email,str_address,str_username,str_password);
+            if (test==true)
+            {
+                Toast.makeText(getContext(), "done", Toast.LENGTH_SHORT).show();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.mainlayout, new Fragment_Sign_in());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+            else {
+                Toast.makeText(getContext(), "not done", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
     }
 }
